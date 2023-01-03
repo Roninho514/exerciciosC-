d1023bdb2d8a415f962c52d129f9d265937371a6
Roninho514 <roni.silvasantos514@gmail.com>
2023-01-03 20:58:34 -0300

    Exercício feito 03/01/23

---
+++ TabuadaRepeticao/TabuadaRepeticao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabuadaRepeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int contador = 1; contador <= 10; contador ++)
+            {
+                for(int numerador = 1; numerador <=10; numerador++)
+                {
+                    int result = contador * numerador;
+                    Console.WriteLine("{0} x {1} = {2}" , contador, numerador,result);
+                }
+
+                Console.WriteLine("--------------------");
+
+            }
+                Console.ReadLine();
+        }
+    }
+}
