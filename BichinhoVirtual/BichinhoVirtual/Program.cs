0958823fb8c2a588f5d3572c7a2435476bd7c7da
Roninho514 <roni.silvasantos514@gmail.com>
2023-02-18 01:20:51 -0300

    Add request GET API pokeapi

---
+++ BichinhoVirtual/BichinhoVirtual/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Serializers;
+
+namespace BichinhoVirtual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ApiPokemons = new RestClient("https://pokeapi.co/api/v2/pokemon/");
+            RestRequest request = new RestRequest("",Method.Get);
+            var response = ApiPokemons.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine(response.Content);
+            }
+            else
+            {
+                Console.WriteLine(response.ErrorMessage);
+            }
+            Console.ReadLine();
+        }
+    }
+}
